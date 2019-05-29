82e9f2df89c84f3aba236af9e48cd009947ef3b8
artem <40721180+nichtiak@users.noreply.github.com>
2019-05-29 18:25:48 +0500

    Add files via upload

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -364,31 +364,29 @@ module.exports = form;
 /***/ (function(module, exports) {
 
 function images() {
-    let imgMin = document.querySelectorAll('.img_min'),
-        imgBig = document.querySelectorAll('.works a'),
-        img = document.createElement('img');
-        imgBlock = document.createElement ('div');
-        
-        imgBlock.classList.add('overlay');
-        imgBlock.classList.add('imgModal');
+    let photoSmall = document.querySelectorAll('.photo'),
+        overlay = document.querySelector('.overlay');
 
-        for (let i = 0; i < imgMin.length; i++) {
-            imgMin[i].addEventListener('click', function (e) {
-                e.preventDefault();
-                let target = e.target;
-                for (let a = 0; a < imgBig.length; a++) {
-                    let imgUrl = imgBig[a].getAttribute('href');
-                    if (target == imgBig[a]) {
-                        body.appendChild(imgBlock);
-                        imgBlock.style.display = 'block';
-                        img.setAttribute('src', imgUrl);
-                        let imgModal = document.querySelector('.imgModal');
-                        imgModal.appendChild(img);
-                        console.log(imgModal);
-                    }
-                }
-            });
+    for (let i = 0; i < photoSmall.length; i++) {
+        photoSmall[i].addEventListener('click', () => {
+            event.preventDefault();
+            let div = document.createElement('div'),
+                a = photoSmall[i].parentElement.href;
+            div.innerHTML = photoSmall[i].parentElement.innerHTML;
+            overlay.appendChild(div);
+            overlay.querySelector('img').src = a;
+            overlay.style.cssText = 'display: flex; justify-content: center; align-items: center;';
+            document.body.style.overflow = 'hidden';
+        });
+    }
+
+    window.addEventListener('click', (e) => {
+        if (e.target == overlay) {
+            overlay.style.display = 'none';
+            overlay.innerHTML = '';
+            document.body.style.overflow = '';
         }
+    });
 }
 module.exports = images;
 
